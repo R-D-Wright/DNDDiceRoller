a05df628a3fb2133d97a0cf134bafba19a904b0b
Drake Wright <rdrakewright@gmail.com>
2023-05-11 08:17:01 -0500

    Initial Commit

---
+++ diceroller(3)/diceroller_3_App.swift
@@ -0,0 +1,17 @@
+//
+//  diceroller_3_App.swift
+//  diceroller(3)
+//
+//  Created by Drake Wright on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct diceroller_3_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
